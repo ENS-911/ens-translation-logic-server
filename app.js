1bf49de0136d8384ac1239cdd6f5862a4fbd9ab7
Kenneth Brook <kenneth.brook@hotmail.com>
2023-11-17 14:57:03 -0500

    updated for .env

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const { Client } = require('pg');
 const AWS = require('aws-sdk');
+const dotenv = require('dotenv');
 
 const databaseConfig = {
-    user: 'your_db_user',
-    host: 'your_db_host',
-    database: 'your_db_name',
-    password: 'your_db_password',
-    port: 5432, // Change this according to your PostgreSQL setup
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 };
+  
+const lambdaConfig = {
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+};
+  
+const postgresClient = new Client(databaseConfig);
+const lambda = new AWS.Lambda(lambdaConfig);
