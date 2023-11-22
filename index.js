e3f81d9f1365be08a726d5159138b1666a34d580
Kenneth Brook <kenneth.brook@hotmail.com>
2023-11-22 14:17:56 -0500

    postgres adjustment

--- index.js
+++ index.js
@@ -3,22 +3,12 @@ const AWS = require('aws-sdk');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const databaseConfig = {
-  user: 'ensclient',
-    host: 'ens-client.cfzb4vlbttqg.us-east-2.rds.amazonaws.com',
-    database: 'ens-client',
-    password: 'gQ9Sf8cIczKhZiCswXXy',
-    port: 5432,
-  max: 10, // Adjust as needed for connection pooling
-};
-
 const lambdaConfig = {
   region: process.env.AWS_REGION,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 };
 
-const postgresClient = new Client(databaseConfig);
 const lambda = new AWS.Lambda(lambdaConfig);
 
 const databaseTypeToLambdaMap = {
@@ -31,6 +21,17 @@ async function processDatabaseRow(row) {
   const { db_type, dbsync } = row;
 
   if (dbsync.toLowerCase() === 'yes' && databaseTypeToLambdaMap.hasOwnProperty(db_type)) {
+    const databaseConfig = {
+      user: 'ensclient',
+      host: 'ens-client.cfzb4vlbttqg.us-east-2.rds.amazonaws.com',
+      database: 'ens-client',
+      password: 'gQ9Sf8cIczKhZiCswXXy',
+      port: 5432,
+      max: 10, // Adjust as needed for connection pooling
+    };
+
+    const postgresClient = new Client(databaseConfig);
+
     const lambdaFunctionName = databaseTypeToLambdaMap[db_type];
     const lambdaParams = {
       FunctionName: lambdaFunctionName,
@@ -39,16 +40,22 @@ async function processDatabaseRow(row) {
     };
 
     try {
+      await postgresClient.connect();
+
       const lambdaResponse = await lambda.invoke(lambdaParams).promise();
       console.log('Lambda Response:', lambdaResponse);
     } catch (error) {
       console.error('Error invoking Lambda function:', error);
       // Log the error and continue with the next row
+    } finally {
+      await postgresClient.end();
     }
   }
 }
 
 async function main() {
+  const postgresClient = new Client(); // Create a new client instance for the main function
+
   try {
     await postgresClient.connect();
 
